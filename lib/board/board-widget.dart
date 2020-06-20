ffa019a87ab544dcddcbbc495b41a27c9b93f086
YoungPeak <biocc06@163.com>
2020-06-21 00:08:04 +0800

    完善棋盘绘制

--- lib/board/board-widget.dart
+++ lib/board/board-widget.dart
@@ -3,6 +3,8 @@ import 'package:chessboard/board/board-paint.dart';
 import 'package:chessboard/common/color-consts.dart';
 import 'package:flutter/material.dart';
 
+import 'words-on-board.dart';
+
 class BoardWidget extends StatelessWidget {
 
   // 棋盘内边界 + 棋盘上的路数指定文字高度
@@ -25,6 +27,15 @@ class BoardWidget extends StatelessWidget {
       ),
       child: CustomPaint(
         painter: BoardPainter(width: width),
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            vertical: Padding,
+            // 因为棋子是放在交叉线上的，不是放置在格子内，所以棋子左右各有一半在格线之外
+            // 这里先依据横盘的宽度计算出一个格子的边长，再依此决定垂直方向的边距
+            horizontal: (width - Padding * 2) / 9 / 2 + Padding - WordsOnBoard.DigitsFontSize / 2,
+          ),
+          child: WordsOnBoard(),
+        ),
       ),
     );
   }
---
+++ lib/board/words-on-board.dart
@@ -0,0 +1,55 @@
+
+import 'package:chessboard/common/color-consts.dart';
+import 'package:flutter/material.dart';
+
+class WordsOnBoard extends StatelessWidget {
+
+  static const DigitsFontSize = 18.0;
+
+  @override
+  Widget build(BuildContext context) {
+    // 棋盘上的路数指示，红方用大写，黑方用全角的小写字母
+    final blackColumns = '１２３４５６７８９', redColumns = '九八七六五四三二一';
+    final bChildren = <Widget>[], rChildren = <Widget>[];
+
+    final digitsStyle = TextStyle(fontSize: DigitsFontSize);
+    final riverTipStyle = TextStyle(fontSize: 28.0);
+
+    for (var i = 0; i < 9; i++) {
+      bChildren.add(Text(blackColumns[i], style: digitsStyle));
+      rChildren.add(Text(redColumns[i], style: digitsStyle));
+
+      // 每一个数字后边添加一个Expanded，用于平分布局空间
+      if (i < 8) {
+        bChildren.add(Expanded(child: SizedBox()));
+        rChildren.add(Expanded(child: SizedBox()));
+      }
+    }
+
+    final riverTips = Row(
+      children: <Widget>[
+        Expanded(child: SizedBox()),
+        Text('楚河', style: riverTipStyle),
+        Expanded(child: SizedBox(), flex: 2),
+        Text('汉界', style: riverTipStyle),
+        Expanded(child: SizedBox())
+      ],
+    );
+
+    // 放置上、中、下三部分到一个列布局模式中，上和中、中和下之间用Expanded对象
+    // 平分垂直方向多出来的布局空间
+    return DefaultTextStyle(
+      child: Column(
+        children: <Widget>[
+          Row(children: bChildren),
+          Expanded(child: SizedBox()),
+          riverTips,
+          Expanded(child: SizedBox()),
+          Row(children: rChildren)
+        ],
+      ),
+      style: TextStyle(color: ColorConsts.BoardTips),
+    );
+  }
+
+}
